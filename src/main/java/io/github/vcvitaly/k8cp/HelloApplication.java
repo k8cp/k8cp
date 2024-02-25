ea241718163b7f163521a8ca308e4137e698ff62
Vitalii Chura <vcvitaly@yahoo.com>
2024-02-25 19:17:25 +0200

    Align the VBoxes

--- src/main/java/io/github/vcvitaly/k8cp/HelloApplication.java
+++ src/main/java/io/github/vcvitaly/k8cp/HelloApplication.java
@@ -2,6 +2,7 @@ package io.github.vcvitaly.k8cp;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -11,7 +12,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 640, 480);
+        Parent parent = fxmlLoader.load();
+        Scene scene = new Scene(parent, 640, 480);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
