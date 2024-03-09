8cfbc3aa3a782b208a1118119a427bf015d802c1
VItalii Chura <vcvitaly@yahoo.com>
2024-03-09 16:48:03 +0200

    add fx ids to buttons

--- src/main/java/io/github/vcvitaly/k8cp/controller/MainController.java
+++ src/main/java/io/github/vcvitaly/k8cp/controller/MainController.java
@@ -1,14 +1,31 @@
 package io.github.vcvitaly.k8cp.controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
-public class MainController {
-    @FXML
-    private Label welcomeText;
+public class MainController implements Initializable {
+    public Button parentLeftBtn;
+    public Button homeLeftBtn;
+    public Button refreshLeftBtn;
+    public Button copyLeftBtn;
+    public Button moveLeftBtn;
+    public Button deleteLeftBtn;
+    public Button renameLeftBtn;
+    public Button parentRightBtn;
+    public Button homeRightBtn;
+    public Button refreshRightBtn;
+    public Button copyRightBtn;
+    public Button moveRightBtn;
+    public Button deleteRightBtn;
+    public Button renameRightBtn;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 }
